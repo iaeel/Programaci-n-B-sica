3c621b7b72273509c45e81f57a372dd208344efb
iaeel <97769623+iaeel@users.noreply.github.com>
2025-01-27 23:45:38 -0600

    Add files via upload

---
+++ ACT3.py
@@ -0,0 +1,27 @@
+print("hola mundo")
+# ¿cual es la funcion de la memoria secundiaria en una computadora?
+#c) almacenar informacion durante mucho tiempo, incluso despues de ciclos de apagado y encendido
+# Ejercicio 2
+# ¿que es un programa?
+#conjunto de pasos lógicos escritos en un lenguaje de programación que nos permite realizar una tarea específica
+# Ejercicio 3
+# ¿cual es la diferencia entre un compilador y un interprete?
+#Un compilador traduce un programa a un lenguaje de máquina, mientras que un intérprete traduce y ejecuta el código línea por línea.
+# Ejercicio 4
+# ¿cuál de los siguientes contiene "codigo maquina"? a) el interprete de Python
+# Ejercicio 5
+# ¿que esta mal en el siguiente codigo?
+#primt  ' ¡hola, mundo! '
+#file "<stdin>", line 1
+#primt  ' ¡hola, mundo! '
+# repuesta: el codigo deberia ser print("hola mundo")
+# ejercicio 6
+# ¿ en que lugar del computador queda almacenada una variable, como en esto caso "x", despues de ejecutar la siguiente linea de Python?
+#x= 123
+#b) memoria principal
+#ejercicio 7
+# ¿que mostrara en la pantalla el siguiente programa?
+#x = 43
+#x = x + 1
+#print(x)
+#b) 44
